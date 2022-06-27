e13073e2f3bc09f6d96a466bf5e0a768239f9a0f
Gumiho <lukaslaschitz@gmail.com>
2022-06-27 11:26:44 +0200

    push

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace wpf1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
